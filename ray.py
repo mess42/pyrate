94118824a2d3ca1b05c7fffd77655428f9637ecd
mess42 <moritz.esslinger@web.de>
2014-06-03 19:22:54 +0200

    Update ray.py

--- ray.py
+++ ray.py
@@ -1,24 +1,22 @@
-class Ray(object):
+class RayBundle(object):
     """
-    Class representing a ray.
-    TODO: Maybe it's better to represent a set of rays with common
-            wavelength and polarisation (in order to share common refraction properties),
-            so they can be easier vectorized for parallel processing.
-            self.o and self.d will then be arrays of vectors instead of single 3-element vectors.
+    Class representing a bundle of rays.
     """
     def __init__(self, o, d, wave=0.55, pol=[], n=1.0):
         """
         Constructor defining the ray properties
-        :param o: Origin of the ray
-        :param d: Direction of the ray. The length of the passed vector
-                    sets the propagation length of the ray.
-        :param wave: Wavelength of the radiation in microns.
-        :param pol: Polarization state of the ray.
-        :param n: Refractive index of the medium the ray is created in.
+        :param o:    Origin of the rays.   (2d numpy 3xN array of float)
+        :param d:    Direction of the rays.(2d numpy 3xN array of float) 
+                     The length of the passed 3D vectors
+                     sets the propagation length of the rays.
+        :param wave: Wavelength of the radiation in microns. (float)
+        :param pol:  Polarization state of the rays. (2d numpy 2xN array of complex)
+        :param n:    Refractive index of the medium the rays are currently in. (float)
                 TODO: Maybe pass a reference to the Material object instead of n.
+                TODO: What happens with n in birefringent materials ?
         """
         self.o = o
-        mag = np.sqrt(np.dot(d, d))
+        mag = sqrt( sum( d**2, axis=0 ) )
         self.d = d / mag
         self.t = mag
         self.wave = wave
