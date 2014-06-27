f4710fb164eac24cc7b0b4c0c59ded4b91a6df62
mess42 <moritz.esslinger@web.de>
2014-06-27 16:48:40 +0200

    parsing shape types

--- demo.py
+++ demo.py
@@ -21,27 +21,27 @@ s.set_thickness( position = 0, thickness = 15 )
 
 s.insert_surface(1)
 s.set_thickness( position = 1, thickness = 5 )
-s.set_material(  position = 1, materialname = "simple test glass")
-#s.surfaces[1].set_shape("Conic")
+s.setMaterial( position = 1, materialType = 'SimpleGlass', materialName = 1.5)
+s.surfaces[1].setShapeType("Conic")
 s.surfaces[1].shap.curvature = 1/15.279
 s.surfaces[1].shap.sdia = 6
 
 s.insert_surface(2)
 s.set_thickness( position = 2, thickness = 50 )
-#s.surfaces[1].set_shape("Conic")
+s.surfaces[1].setShapeType("Conic")
 s.surfaces[2].shap.curvature = -1/9.903
 s.surfaces[2].shap.sdia = 6
 
 #definition of rays
-origin = zeros((3,3), dtype=float )
-k = zeros((3,3), dtype=float )
-k[1,0] =  5./15
-k[1,2] = -5./15
+origin = zeros((3,7), dtype=float )
+k = zeros((3,7), dtype=float )
+k[1,:] = linspace(-5./15, 5./15, 7)
 k[2,:] = 1
 absk = sqrt( sum(k**2, axis=0) )
 k[0] = k[0] / absk
 k[1] = k[1] / absk
 k[2] = k[2] / absk
+
 
 initialraybundle = RayBundle( origin, k, wave=0.55 )
 r = RayPath(initialraybundle, s)
