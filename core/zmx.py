54e1e8cfae8bd96ad5255be16921595dd3a05050
joha2 <demon_ds@umara>
2015-11-25 19:44:41 +0100

    some zmx interpretation code, can now parse surfaces into single dict for every surface

--- core/zmx.py
+++ core/zmx.py
@@ -33,9 +33,11 @@ class ParseZMX(object):
     parm3: rx
     parm4: ry
     parm5: rz
-    parm6: order 0: heisst 1. decx, 2. decy, 3. rx, 4. ry, 5. rz
-            order 1: heisst 1. rz 2. ry 3. rz, 4. decy, 5. decx
+    parm6: order 0: means 1. decx, 2. decy, 3. rx, 4. ry, 5. rz
+            order 1: means 1. rz 2. ry 3. rz, 4. decy, 5. decx
     disz: thickness (where does this appear in the transform? step 0?)
+    all in all: 1st step: transform coordinate system by former disz, dec, tilt (or tilt, dec)
+    2nd step: update vertex
     """
 
     def __init__(self, filename):
@@ -74,14 +76,57 @@ class ParseZMX(object):
 
         return None
 
+    def extractArgsForFirstKeywordFromBlock(self, blocklines, keywordstr, *args):
+        reslist = filter(lambda x: x != None, [p.readArgsForKeyword(lin, keywordstr, *args) for lin in blocklines])
+        if reslist != []:
+            res = reslist[0] # get only first appearence
+        else:
+            res = None
+        return res
+
+    def extractFirstArgForFirstKeywordFromBlock(self, blocklines, keywordstr, *args):
+        res = self.extractArgsForFirstKeywordFromBlock(blocklines, keywordstr, *args)
+        if res != None:
+            res = res[0]
+        return res
+
+
+    def extractArgsForMultipleKeywordFromBlock(self, blocklines, keywordstr, *args):
+        reslist = filter(lambda x: x != None, [p.readArgsForKeyword(lin, keywordstr, *args) for lin in blocklines])
+        if reslist != []:
+            res = reslist # get all appearences
+        else:
+            res = None
+        return res
+
+    def isKeywordInBlock(self, blocklines, keywordstr, *args):
+        return self.extractArgsForFirstKeywordFromBlock(blocklines, keywordstr) != None
+
+
+    def addKeywordToDict(self, blocklines, keywordstr, dicti, funct, *args):
+        res = funct(blocklines, keywordstr, *args)
+        if res != None:
+            dicti[keywordstr] = res
+
     def readSurfBlock(self, surfblk):
-        res = []
-        # res = [surfnum, stop, type, parms, disz]
+        paramsdict = {}
 
         blocklines = [x.lstrip() for x in surfblk.split("\r\n")]
 
 
-        return blocklines
+        self.addKeywordToDict(blocklines, "SURF", paramsdict, self.extractFirstArgForFirstKeywordFromBlock, int)
+        self.addKeywordToDict(blocklines, "STOP", paramsdict, self.isKeywordInBlock)
+        self.addKeywordToDict(blocklines, "TYPE", paramsdict, self.extractFirstArgForFirstKeywordFromBlock, str)
+        self.addKeywordToDict(blocklines, "PARM", paramsdict, self.extractArgsForMultipleKeywordFromBlock, int, float)
+        self.addKeywordToDict(blocklines, "DISZ", paramsdict, self.extractFirstArgForFirstKeywordFromBlock, float)
+
+        self.addKeywordToDict(blocklines, "DIAM", paramsdict, self.extractFirstArgForFirstKeywordFromBlock, float)
+        self.addKeywordToDict(blocklines, "COMM", paramsdict, self.extractFirstArgForFirstKeywordFromBlock, str)
+
+        self.addKeywordToDict(blocklines, "CURV", paramsdict, self.extractFirstArgForFirstKeywordFromBlock, float)
+        self.addKeywordToDict(blocklines, "CONI", paramsdict, self.extractFirstArgForFirstKeywordFromBlock, float)
+
+        return paramsdict
 
 
 
@@ -101,8 +146,8 @@ if __name__ == "__main__":
 
     for blk in surfbs:
         print "----"
-        surflines = p.readSurfBlock(blk)
-        print filter(lambda x: x != None, [p.readArgsForKeyword(lin, "SURF", int) for lin in surflines])
+        surfres = p.readSurfBlock(blk)
+        print surfres #filter(lambda x: x != None, [p.readArgsForKeyword(lin, "SURF", int) for lin in surflines])
 
 
 
