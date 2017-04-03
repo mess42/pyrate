396e2ed24227e07d3880c859b839fbc0840b6daa
t.heinze <t.heinze@uni-jena.de>
2017-04-03 19:50:33 +0200

    smoke test update and appveyor CI

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -4,5 +4,11 @@ Pyrate - Optical raytracing based on Python
 
 def test_smoke_doublet():
     """Smoke test based on demo_doublet.py."""
+
+    # we use the matplotlib's do nothing backend for testing
+    # matplotlib/lib/matplotlib/backends/backend_template.py
+    import matplotlib
+    matplotlib.use('Template')
+
     import demo_doublet
     assert True
