ee2410b57f7e9babf3104a39cbefea6209d929e6
joha2 <joha2@gmx.net>
2017-04-06 22:52:31 +0200

    moved field.py to oldcode

--- core/ray_analysis.py
+++ core/ray_analysis.py
@@ -24,7 +24,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 import numpy as np
 from ray import RayBundle
 
-import math
 
 class RayBundleAnalysis(object):
     def __init__(self, raybundle):
