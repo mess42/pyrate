5196a80d4cfd86ae699a33ca5fa74caf736a4930
foerster <foerster@localhost>
2017-04-20 16:45:09 +0200

    glasscat extenseion and bugfixes

--- core/glasscatalog.py
+++ core/glasscatalog.py
@@ -24,7 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 import yaml
 import numpy as np
-import scipy
+import scipy.interpolate
 
 class refractiveindex_dot_info_glasscatalog(object):
     def __init__(self, database_basepath):
@@ -69,11 +69,27 @@ class refractiveindex_dot_info_glasscatalog(object):
         data = self.read_yml_file(ymlfilename)
         return data
         
-    def getDispersion(self, shelf, book, page):
+    def getDispersionFunctions(self, shelf, book, page):
         data = self.getMaterialData(shelf, book, page)["DATA"]
 
-        raise NotImplementedError()
+        if len(data) > 2:
+            raise Exception("Max 2 entries for dispersion allowed - n and k.")
         
+        nk = []
+        for i in np.arange(len(data)): # i=0 is n  ;  i=1 is k
+            dispersionDict = data[i]
+            typ= dispersionDict["type"]            
+            if dispersionDict["type"].startswith("tabulated"):
+                coeff = dispersionDict["data"].split("\n")[:-1]
+                for j in np.arange(len(coeff)):
+                    coeff[j] = coeff[j].split()
+            else:
+                coeff = dispersionDict["coefficients"].split()
+            coeff = np.array(coeff, dtype=float)
+            nk.append(IndexFormulaContainer(typ, coeff))
+        return nk              
+        
+        #raise NotImplementedError()
         return data
             
     def read_yml_file(self, ymlfilename):
@@ -118,7 +134,7 @@ class refractiveindex_dot_info_glasscatalog(object):
 
         yaml_library = self.read_yml_file(library_yml_filename)
         
-        lib = list2dict(yaml_library, "SHELF")
+        lib = self.list2dict(yaml_library, "SHELF")
         for shelfname in lib:
             lib[shelfname]["content"] = self.list2dict(lib[shelfname]["content"], "BOOK")
             for bookname in lib[shelfname]["content"]:
@@ -128,148 +144,114 @@ class refractiveindex_dot_info_glasscatalog(object):
 
 
 class IndexFormulaContainer(object):
-    def __init__(self, n_typ, n_coeff, k_typ = 0, k_coeff=[] ):
+    def __init__(self, typ, coeff):
         """
         Stores refractive index formula and coefficients.
         
-        :param n_typ: (str)
+        :param typ: (str)
                index dispersion formula name
                Must be according to refractiveindex.info naming scheme.
-        :param k_typ: (str)
-               extinction formula name        
-               Must be according to refractiveindex.info naming scheme.
-        :param n_coeff: (numpy array of float)
+        :param coeff: (numpy array of float)
                index formula coefficients
                Coefficients must be according to refractiveindex.info scheme.
                An in units of micrometers.
                ( Just copy numbers unchanged from the refractiveindex.info
                  yml-database. It'll be fine. )
-        :param k_coeff: (numpy array of float)
-               extinction formula coefficients
-               Coefficients must be according to refractiveindex.info scheme.
-               An in units of micrometers.
         """
-        self.set_n_function(n_typ, n_coeff)
-        self.set_k_function(k_typ, k_coeff)
+        self.setDispFunction(typ, coeff)
         
-    def set_n_function(self, n_typ, n_coeff):
-        self.n_coeff = n_coeff
+    def setDispFunction(self, typ, coeff):
+        self.coeff = coeff
 
         def Sellmeier(w_um):
-            B = self.n_coeff[1::2]
-            C = self.n_coeff[2::2]
-            nsquared = 1 + self.n_coeff[0] + sum( B * w_um**2 / (  w_um**2 - C**2  ) )
+            B = self.coeff[1::2]
+            C = self.coeff[2::2]
+            nsquared = 1 + self.coeff[0] + sum( B * w_um**2 / (  w_um**2 - C**2  ) )
             return np.sqrt(nsquared)
         def Sellmeier2(w_um):
-            B = self.n_coeff[1::2]
-            C = self.n_coeff[2::2]
-            nsquared = 1 + self.n_coeff[0] + sum( B * w_um**2 / (  w_um**2 - C ) )
+            B = self.coeff[1::2]
+            C = self.coeff[2::2]
+            nsquared = 1 + self.coeff[0] + sum( B * w_um**2 / (  w_um**2 - C ) )
             return np.sqrt(nsquared)
         def Polynomial(w_um):
-            A = self.n_coeff[1::2]
-            P = self.n_coeff[2::2]
-            nsquared = self.n_coeff[0] + sum( A * (w_um**P) )
+            A = self.coeff[1::2]
+            P = self.coeff[2::2]
+            nsquared = self.coeff[0] + sum( A * (w_um**P) )
             return np.sqrt(nsquared)
         def refractiveindex_dot_info_formula_with_9_or_less_coefficients(w_um):
-            A = self.n_coeff[1::4]
-            B = self.n_coeff[2::4]
-            C = self.n_coeff[3::4]
-            D = self.n_coeff[4::4]
-            nsquared = self.n_coeff[0] + sum( A * (w_um**B)  / (  w_um**2 - C**D ) )
+            A = self.coeff[1::4]
+            B = self.coeff[2::4]
+            C = self.coeff[3::4]
+            D = self.coeff[4::4]
+            nsquared = self.coeff[0] + sum( A * (w_um**B)  / (  w_um**2 - C**D ) )
             return np.sqrt(nsquared)
         def refractiveindex_dot_info_formula_with_11_or_more_coefficients(w_um):
-            A = self.n_coeff[[1,5]]
-            B = self.n_coeff[[2,6]]
-            C = self.n_coeff[[3,7]]
-            D = self.n_coeff[[4,8]]
-            E = self.n_coeff[9::2]
-            F = self.n_coeff[10::2]
-            nsquared = self.n_coeff[0] + sum( A * (w_um**B)  / (  w_um**2 - C**D ) ) + sum( E * (w_um**F) )
+            A = self.coeff[[1,5]]
+            B = self.coeff[[2,6]]
+            C = self.coeff[[3,7]]
+            D = self.coeff[[4,8]]
+            E = self.coeff[9::2]
+            F = self.coeff[10::2]
+            nsquared = self.coeff[0] + sum( A * (w_um**B)  / (  w_um**2 - C**D ) ) + sum( E * (w_um**F) )
             return np.sqrt(nsquared)
         def Cauchy(w_um):
-            A = self.n_coeff[1::2]
-            P = self.n_coeff[2::2]
-            n = self.n_coeff[0] + sum( A * (w_um**P) )
+            A = self.coeff[1::2]
+            P = self.coeff[2::2]
+            n = self.coeff[0] + sum( A * (w_um**P) )
             return n
         def Gases(w_um):
-            B = self.n_coeff[1::2]
-            C = self.n_coeff[2::2]
-            n = 1 + self.n_coeff[0] + sum( B / (  C - w_um**(-2)  ) )
+            B = self.coeff[1::2]
+            C = self.coeff[2::2]
+            n = 1 + self.coeff[0] + sum( B / (  C - w_um**(-2)  ) )
             return n
         def Herzberger(w_um):
             denom = w_um**2 - 0.028
-            A = self.n_coeff[3:]
+            A = self.coeff[3:]
             P = 2 * np.arange(len(A)) + 2
-            n = self.n_coeff[0] + self.n_coeff[1] / denom + self.n_coeff[2] / (denom**2) + sum (A * w_um**P )
+            n = self.coeff[0] + self.coeff[1] / denom + self.coeff[2] / (denom**2) + sum (A * w_um**P )
             return n
         def Retro(w_um):
             raise NotImplementedError()
         def Exotic(w_um):
             raise NotImplementedError()
 
-        if n_typ == "tabulated n":
-            self.n_function = scipy.interpolate.interp1d(self.n_coeff[0,:], self.n_coeff[1,:])
-        elif n_typ == "formula  1":
-            self.n_function = Sellmeier
-        elif n_typ == "formula  2":
-            self.n_function = Sellmeier2
-        elif n_typ == "formula  3":
-            self.n_function = Polynomial
-        elif n_typ == "formula  4":
-            if len(self.n_coeff) > 10:
-                self.n_function = refractiveindex_dot_info_formula_with_11_or_more_coefficients
+        if   typ == "formula 1":
+            self.__dispFunction = Sellmeier
+        elif typ == "formula 2":
+            self.__dispFunction = Sellmeier2
+        elif typ == "formula 3":
+            self.__dispFunction = Polynomial
+        elif typ == "formula 4":
+            if len(self.coeff) > 10:
+                self.__dispFunction = refractiveindex_dot_info_formula_with_11_or_more_coefficients
             else:
-                self.n_function = refractiveindex_dot_info_formula_with_9_or_less_coefficients
-        elif n_typ == "formula  5":
-            self.n_function = Cauchy
-        elif n_typ == "formula  6":
-            self.n_function = Gases
-        elif n_typ == "formula  7":
-            self.n_function = Herzberger
-        elif n_typ == "formula  8":
+                self.__dispFunction = refractiveindex_dot_info_formula_with_9_or_less_coefficients
+        elif typ == "formula 5":
+            self.__dispFunction = Cauchy
+        elif typ == "formula 6":
+            self.__dispFunction = Gases
+        elif typ == "formula 7":
+            self.__dispFunction = Herzberger
+        elif typ == "formula 8":
             self.n_fucntion = Retro
-        elif n_typ == "formula  9":
-            self.n_function = Exotic
-
-    def set_k_function(self, k_typ, k_coeff):
-        self.k_coeff = k_coeff
-
-        def Lossless(w_um):
-            return 0
-            
-        if k_typ == "tabulated k":
-            self.k_function = scipy.interpolate.interp1d(self.k_coeff[0,:], self.k_coeff[1,:])
+        elif typ == "formula 9":
+            self.__dispFunction = Exotic
+        elif typ == "tabulated n":
+            self.__dispFunction = scipy.interpolate.interp1d(self.coeff[:,0],      self.coeff[:,1])
+        elif typ == "tabulated k":
+            self.__dispFunction = scipy.interpolate.interp1d(self.coeff[:,0], 1j * self.coeff[:,1])
         else:
-            self.k_function = Lossless
+            raise Exception("Bad dispersion function type: "+str(typ))
             
-    def get_n(self, wavelength):
+    def getDispersion(self, wavelength):
         """
         :param wavelength: (float)
                wavelength in mm
         :return n: (float)
                refractive index real part
         """
-        return self.n_function(w_um = 0.001 * wavelength)
-        
-    def get_k(self, wavelength):
-        """
-        :param wavelength: (float)
-               wavelength in mm
-        :return k: (float)
-               refractive index imaginary part
-               (extinction coefficient)
-        """
-        return self.k_function(w_um = 0.001 * wavelength)
-
-    def get_complex_epsilon(w_um):
-        """
-        :param wavelength: (float)
-               wavelength in mm
-        :return epsilon: (complex)
-               permittivity
-        """
-        return ( self.n_function(w_um = 0.001 * wavelength) + 1j * self.k_function(w_um = 0.001 * wavelength) )**2
-        
+        return self.__dispFunction(1000 * wavelength)
+                
 
 if __name__ == "__main__":
 
@@ -284,8 +266,11 @@ if __name__ == "__main__":
     print "Pages in BK7 book:", gcat.getPages(shelf = "glass", book="BK7")
     print ""
     print "Long name of SCHOTT page is:", gcat.getPageLongName(shelf = "glass", book = "BK7", page = "SCHOTT")
+    print ""
     
-    schottNBK7 = gcat.getDispersion(shelf = "glass", book = "BK7", page = "SCHOTT")
+    schottNBK7 = gcat.getDispersionFunctions(shelf = "glass", book = "BK7", page = "SCHOTT")
+    print "nd = ", schottNBK7[0].getDispersion(587.6E-6)
+    print "k  = ", schottNBK7[1].getDispersion(587.6E-6)
 
 
 
