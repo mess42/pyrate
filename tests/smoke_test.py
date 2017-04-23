37d7062856e30f4ae222c9968c48c05e1d4e02d3
theinze <t.heinze@uni-jena.de>
2017-04-23 22:52:39 +0200

    added demo_asphere smoke test

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -31,27 +31,30 @@ __author__ = 'Thomas Heinze'
 # matplotlib/lib/matplotlib/backends/backend_template.py
 pyplot.switch_backend('Template')
 
+def test_smoke_asphere():
+    """Smoke test based on demo_asphere.py."""
+    import demos.demo_asphere
+    assert True
+def test_smoke_benchmark():
+    """Smoke test based on demo_benchmark.py."""
+    import demos.demo_benchmark
+    assert True
 @image_comparison(baseline_images=['doublet'], extensions=['png'])
 def test_smoke_doublet():
     """Smoke test based on demo_doublet.py."""
     import demos.demo_doublet
     assert True
-
-def test_smoke_benchmark():
-    """Smoke test based on demo_benchmark.py."""
-    import demos.demo_benchmark
+def test_smoke_grin():
+    """Smoke test based on demo_grin.py."""
+    import demos.demo_grin
     assert True
-def test_smoke_mirror():
+def test_smoke_mirrors():
     """Smoke test based on demo_mirrors.py."""
     import demos.demo_mirrors
     assert True
 def test_smoke_optimize():
     """Smoke test based on demo_optimize.py."""
     import demos.demo_optimize
-    assert True
-def test_smoke_grin():
-    """Smoke test based on demo_grin.py."""
-    import demos.demo_grin
     assert True
 def test_smoke_prism():
     """Smoke test based on demo_prism.py."""
