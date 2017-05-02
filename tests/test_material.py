48fafdf12617d1f169cec1f75c428d885e4ebfba
joha2 <joha2@gmx.net>
2017-05-02 10:15:35 +0200

    added try block for sympy test

--- tests/test_material.py
+++ tests/test_material.py
@@ -24,7 +24,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 import numpy as np
 from core.localcoordinates import LocalCoordinates
 from core.material import AnisotropicMaterial
-import sympy
 
 def test_anisotropic_xi_calculation_polynomial():
     '''
@@ -198,29 +197,34 @@ def test_anisotropic_xi_eigenvectors():
     m.calcXiEigenvectors(x, n, kpa)
     
     # sympy check with analytical solution
-    kx, ky, xi = sympy.symbols('k_x k_y xi')
-    exx, exy, exz, eyx, eyy, eyz, ezx, ezy, ezz \
-        = sympy.symbols('e_xx e_xy e_xz e_yx e_yy e_yz e_zx e_zy e_zz')
-    
-    #eps = Matrix([[exx, exy, exz], [eyx, eyy, eyz], [ezx, ezy, ezz]])
-    eps = sympy.Matrix([[exx, exy, 0], [eyx, eyy, 0], [0, 0, ezz]])
-    v = sympy.Matrix([[kx, ky, xi]])
-    m = -(v*v.T)[0]*sympy.eye(3) + v.T*v + eps
-    detm = m.det().collect(xi)
-    
-    soldetm = sympy.solve(detm, xi)
-    subsdict = {
-        kx:kpa[0, 0],
-        ky:kpa[1,0],
-        exx:epsxx,
-        exy:epsxy,
-        eyx:epsyx,
-        eyy:epsyy,
-        ezz:epszz,
-        }
-    for sol in soldetm:
-        print(sol.evalf(subs=subsdict))
- 
+    try:
+        import sympy
+        kx, ky, xi = sympy.symbols('k_x k_y xi')
+        exx, exy, exz, eyx, eyy, eyz, ezx, ezy, ezz \
+            = sympy.symbols('e_xx e_xy e_xz e_yx e_yy e_yz e_zx e_zy e_zz')
+        
+        #eps = Matrix([[exx, exy, exz], [eyx, eyy, eyz], [ezx, ezy, ezz]])
+        eps = sympy.Matrix([[exx, exy, 0], [eyx, eyy, 0], [0, 0, ezz]])
+        v = sympy.Matrix([[kx, ky, xi]])
+        m = -(v*v.T)[0]*sympy.eye(3) + v.T*v + eps
+        detm = m.det().collect(xi)
+        
+        soldetm = sympy.solve(detm, xi)
+        subsdict = {
+            kx:kpa[0, 0],
+            ky:kpa[1,0],
+            exx:epsxx,
+            exy:epsxy,
+            eyx:epsyx,
+            eyy:epsyy,
+            ezz:epszz,
+            }
+        for sol in soldetm:
+            print(sol.evalf(subs=subsdict))
+    except:
+        pass
+    else:
+        pass
     
     
 if __name__=="__main__":
