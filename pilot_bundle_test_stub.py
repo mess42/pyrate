24ca54f4aba49504b1ae0c4d2036154cf73b5901
joha2 <joha2@gmx.net>
2017-05-27 18:15:39 +0200

    some tidying up in pilot bundle stub

--- pilot_bundle_test_stub.py
+++ pilot_bundle_test_stub.py
@@ -57,59 +57,93 @@ if __name__=="__main__":
     #rnd_vecs[2, :] = 1
     rnd_units = rnd_vecs/np.linalg.norm(rnd_vecs, axis=0)
 
-    rnd_data1 = np.random.random((3, 3)) #np.eye(3)
-    rnd_data2 = np.random.random((3, 3))#np.zeros((3, 3))#
+    #rnd_data1 = np.random.random((3, 3)) #np.eye(3)
+    #rnd_data2 = np.random.random((3, 3))#np.zeros((3, 3))#
+
+    rnd_data1 = np.eye(3)
+    rnd_data2 = np.zeros((3, 3))
 
     lc = LocalCoordinates("1")
     myeps = rnd_data1 + complex(0, 1)*rnd_data2
 
+    efield = np.zeros((3, num_pts))
+    efield[0, :] = 1
+    mat_ez = np.zeros((3, num_pts))
+    mat_ez[2, :] = 1
     mat = AnisotropicMaterial(lc, myeps)
 
     kvectorsmat = mat.calcKNormfromUnitVector(np.zeros((3, num_pts)), rnd_units)
 
+    sol_choice = np.zeros(4, dtype=bool)
+
     for i in range(4):
         print("solution n0 %d" % (i,))
-        kvec = np.copy(kvectorsmat[i])
-        
-        rotationrandom = core.helpers_math.random_rotation_matrix(3)    
-        #print(np.dot(np.conj(rotationrandom).T, rotationrandom))    
-        
-        print(rotationrandom)
-        rnd_units_turned = np.einsum("ij...,j...", rotationrandom, rnd_units).T
+        kvecsol = np.copy(kvectorsmat[i])
 
-        kvec_turned = choose_nearest(kvec, mat.calcKNormfromUnitVector(np.zeros((3, num_pts)), rnd_units_turned))
-
-        print(kvec)
-        print(kvec_turned)
+        Svec = mat.calcPoytingVectorNorm(kvecsol, efield)
+        SvecDir = Svec/np.linalg.norm(Svec, axis=0)
+        scalar_product = np.sum(mat_ez*SvecDir, axis=0)
+        sol_choice[i] = scalar_product > 0
+        print(scalar_product)
+        
+    print(kvectorsmat[sol_choice])
+    kvec = kvectorsmat[sol_choice][0]
+        
+    angx = 5.*math.pi/180.0
+    angy = 5.*math.pi/180.0        
+    
+    rotx = core.helpers_math.rodrigues(angx, [1, 0, 0])
+    roty = core.helpers_math.rodrigues(angy, [0, 1, 0])        
             
-        print("det: ", mat.calcDetPropagatorNorm(kvec))
-        print("det: ", mat.calcDetPropagatorNorm(kvec_turned))
-        # observation: just rotation only works with isotropic epsilon (which is invariant under rotation)        
-                
-        print("det derivative")
-        dDdk = mat.calcDetDerivativePropagatorNorm(kvec)
-        print(dDdk)
-        ex = np.repeat(canonical_ex[:, np.newaxis], num_pts, axis=1)
-        ey = np.repeat(canonical_ey[:, np.newaxis], num_pts, axis=1)
-        
-        dky_dir = np.cross(ex, dDdk, axisa=0, axisb=0).T
-        dkx_dir = np.cross(ey, dDdk, axisa=0, axisb=0).T
-        
-        dkx = choose_nearest(kvec, mat.calcKNormfromKNormAndDeviationDirectionVector(np.zeros((3, num_pts)), kvec, dkx_dir))       
-        dky = choose_nearest(kvec, mat.calcKNormfromKNormAndDeviationDirectionVector(np.zeros((3, num_pts)), kvec, dky_dir))       
-                
-        print("det: ", mat.calcDetPropagatorNorm(dkx))
-        print("det: ", mat.calcDetPropagatorNorm(dky))
-        
+    rnd_units_rx = np.einsum("ij...,j...", rotx, rnd_units).T
+    rnd_units_ry = np.einsum("ij...,j...", roty, rnd_units).T
 
-        print("det 2nd derivative")
+
+    kvec_turned_x = choose_nearest(kvec, mat.calcKNormfromUnitVector(np.zeros((3, num_pts)), rnd_units_rx))
+    kvec_turned_y = choose_nearest(kvec, mat.calcKNormfromUnitVector(np.zeros((3, num_pts)), rnd_units_ry))
+
+            
+    print("det derivative")
+    dDdk = mat.calcDetDerivativePropagatorNorm(kvec)
+    print(dDdk)
+    print(np.linalg.norm(dDdk, axis=0))
+    ex = np.repeat(canonical_ex[:, np.newaxis], num_pts, axis=1)
+    ey = np.repeat(canonical_ey[:, np.newaxis], num_pts, axis=1)
+    
+    dky_dir = np.cross(ex, dDdk, axisa=0, axisb=0).T
+    dkx_dir = np.cross(ey, dDdk, axisa=0, axisb=0).T
+    
+    dkx = choose_nearest(kvec, mat.calcKNormfromKNormAndDeviationDirectionVector(np.zeros((3, num_pts)), kvec, dkx_dir))       
+    dky = choose_nearest(kvec, mat.calcKNormfromKNormAndDeviationDirectionVector(np.zeros((3, num_pts)), kvec, dky_dir))       
+
         
-        der2nd = mat.calcDet2ndDerivativePropagatorNorm(kvec)
-        print(der2nd) # matrix somehow wrong oriented (is Nx3x3 insteadt of 3x3xN)
-        print(np.shape(der2nd))
-        (ev, evec) = np.linalg.eig(der2nd[0, :, :])
+    print("det kvec: ", mat.calcDetPropagatorNorm(kvec))
+    print("det kvec_rot_x: ", mat.calcDetPropagatorNorm(kvec_turned_x))
+    print("det kvec_rot_y: ", mat.calcDetPropagatorNorm(kvec_turned_y))
+    # observation: just rotation only works with isotropic epsilon (which is invariant under rotation)        
+            
+    print("det dkx: ", mat.calcDetPropagatorNorm(dkx))
+    print("det dky: ", mat.calcDetPropagatorNorm(dky))
+    
+    print(kvec)
+    print(kvec_turned_x)
+    print(kvec_turned_y)
+    print(dkx)
+    print(dky)
+
+
+    print("det 2nd derivative")
+    
+    der2nd = mat.calcDet2ndDerivativePropagatorNorm(kvec)
+    eigenvecs = np.zeros((2, 3, num_pts), dtype=complex)
+    for i in range(num_pts):
+        (ev, evec) = np.linalg.eig(der2nd[:, :, i])
         print(ev)
-        print(evec)        
+        eigenvecs[:, :, i] = evec[np.abs(ev) < 1e-3]
+        print(evec)
+
+    # TODO: transfer from 1 surf to another, extract 1st order properties
+    # of transfer (automated differentiation)    
 
 
 
