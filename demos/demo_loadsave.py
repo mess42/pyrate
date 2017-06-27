71f7698b32e056672350a471ff2d6bd1212c0d77
joha2 <joha2@gmx.net>
2017-06-27 12:48:56 +0200

    demos: removed effl (obsolete), fixed loadsave (jsonpickle)

--- demos/demo_loadsave.py
+++ demos/demo_loadsave.py
@@ -22,78 +22,19 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
 
-import pickle, pickletools
+import jsonpickle
 
-from core import material
-from core import surfShape
-from core.optical_system import OpticalSystem, Surface
-
-from core.aperture import CircularAperture, BaseAperture
-from core.coordinates import LocalCoordinates
-
-import math
+from core.optical_system import OpticalSystem
 
 # definition of optical system
-s = OpticalSystem() 
 
-lc1 = s.addLocalCoordinateSystem(LocalCoordinates(name="surf1", decz=2.0)) # objectDist
-lc2 = s.addLocalCoordinateSystem(LocalCoordinates(name="surf2", decz=3.0))
-lc3 = s.addLocalCoordinateSystem(LocalCoordinates(name="surf3", decz=5.0, tiltx=2.5*math.pi/180.0))
-lc4 = s.addLocalCoordinateSystem(LocalCoordinates(name="surf4", decz=3.0))
-lc5 = s.addLocalCoordinateSystem(LocalCoordinates(name="surf5", decz=3.0))
-lc6 = s.addLocalCoordinateSystem(LocalCoordinates(name="surf6", decz=2.0))
-lc7 = s.addLocalCoordinateSystem(LocalCoordinates(name="surf7", decz=3.0))
-lc8 = s.addLocalCoordinateSystem(LocalCoordinates(name="image", decz=19.0))
-
-
-s.insertSurface(1, Surface(lc1, surfShape.Conic(curv=1/-5.922), # thickness=3.0,
-                           material=material.ConstantIndexGlass(1.7), 
-                            aperture=BaseAperture()))
-
-s.insertSurface(2, Surface(lc2, surfShape.Conic(curv=1/-3.160), # thickness=5.0, 
-                           aperture=BaseAperture()))
-
-s.insertSurface(3, Surface(lc3, surfShape.Conic(curv=1/15.884), #thickness=3.0,
-                           material=material.ConstantIndexGlass(1.7), 
-                            aperture=BaseAperture()))
-
-s.insertSurface(4, Surface(lc4, surfShape.Conic(curv=1/-12.756), #thickness=3.0,
-                           aperture=BaseAperture()))
-
-s.insertSurface(5, Surface(lc5, surfShape.Conic(), #thickness=2.0, 
-                           aperture=BaseAperture())) # Stop Surface
-
-s.insertSurface(6, Surface(lc6, surfShape.Conic(curv=1/3.125), #thickness=3.0,
-                           material=material.ConstantIndexGlass(1.5), 
-                            aperture=BaseAperture()))
-
-s.insertSurface(7, Surface(lc7, surfShape.Conic(curv=0.1*1/1.479), #thickness=19.0,
-                           aperture=BaseAperture()))
-
-
-s.insertSurface(8, Surface(lc8)) # image
-
-# reintroduce circular apertures
-
-s.surfaces[1].aperture = CircularAperture(0.55)
-s.surfaces[2].aperture = CircularAperture(1.0)
-s.surfaces[3].aperture = CircularAperture(1.3)
-s.surfaces[4].aperture = CircularAperture(1.3)
-s.surfaces[5].aperture = CircularAperture(1.01)
-s.surfaces[6].aperture = CircularAperture(1.0)
-s.surfaces[7].aperture = CircularAperture(1.0)
-
+s = OpticalSystem()
 
 print "pickle dump"
-picklestr = pickle.dumps(s)
-pickletools.dis(picklestr)
+frozen = jsonpickle.encode(s)
 
+with open('optical_sys.jpkl', 'w') as output:
+    output.write(frozen)
 
-with open('optical_sys.pkl', 'wb') as output:
-    pickle.dump(s, output, pickle.HIGHEST_PROTOCOL)
-
-# print introspection variables
-for (s_dict_keys, s_dict_items) in s.__dict__.iteritems():
-    print s_dict_keys, " ", s_dict_items
 
 # WARNING: code is operational, but not tested
