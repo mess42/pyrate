6876b7a2715031a74938fd8aa62080200a95c023
joha2 <joha2@gmx.net>
2017-07-13 11:51:38 -0600

    optical_system_analysis: added file for analysis

---
+++ core/optical_system_analysis.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env/python
+"""
+Pyrate - Optical raytracing based on Python
+
+Copyright (C) 2014 Moritz Esslinger moritz.esslinger@web.de
+               and    Johannes Hartung j.hartung@gmx.net
+               and    Uwe Lippmann  uwe.lippmann@web.de
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+
+import numpy as np
+
+
+class OpticalSystemAnalysis:
+    
+    def __init__(self, os):
+        self.opticalsystem = os
+        
+    def trace(self, pilotbundle, fullsequence):
+        
+        # FIXME: to many parameters in call, maybe set pilotbundle and background medium in advance        
+        list_of_raypaths = []
+        return list_of_raypaths
+        
+    def getFootprint(self, raypath, fulsequence, hitlist_part):
+        
+        # use hitlist_part to select raypath part
+
+        xpos_in_surface_lc = np.array([0, 0])
+
+        return xpos_in_surface_lc
