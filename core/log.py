bdb7017c31d9259e8e3ecaf8954900fda036d5ee
joha2 <joha2@gmx.net>
2017-07-13 11:33:06 -0600

    logging: added file

---
+++ core/log.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env/python
+"""
+Pyrate - Optical raytracing based on Python
+
+Copyright (C) 2014 Moritz Esslinger moritz.esslinger@web.de
+               and    Johannes Hartung j.hartung@gmx.net
+               and    Uwe Lippmann  uwe.lippmann@web.de
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+
+
+import logging
+import uuid
+
+class BaseLogger(object):
+    
+    def __init__(self, name=''):
+        self.setName(name)
+        self.logger = logging.getLogger(name=self.__name)
+        self.info("logger " + name + "created")
+                
+    def setName(self, name):
+        if name == "":
+            name = str(uuid.uuid4())
+        self.__name = name
+        
+    def getName(self):
+        return self.__name
+        
+    name = property(getName, setName)
+    
+    def info(self, msg, *args, **kwargs):
+        self.logger.info(msg, *args, **kwargs)
+    
+    def debug(self, msg, *args, **kwargs):
+        self.logger.debug(msg, *args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        self.logger.error(msg, *args, **kwargs)
+
+    def critical(self, msg, *args, **kwargs):
+        self.logger.critical(msg, *args, **kwargs)
