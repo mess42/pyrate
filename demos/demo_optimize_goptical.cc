d5de5994e8b8b206eac82229042245a23054d90a
joha2 <joha2@gmx.net>
2019-03-05 00:28:27 +0100

    demos: got goptical demo working -> now add benchmark system and test results

--- demos/demo_optimize_goptical.cc
+++ demos/demo_optimize_goptical.cc
@@ -1,64 +1,68 @@
 #include <iostream>
 #include <fstream>
 
-#include <Goptical/Math/Vector>
+#include <goptical/core/math/Vector>
 
-#include <Goptical/Material/Base>
-#include <Goptical/Material/Sellmeier>
+#include <goptical/core/material/Base>
+#include <goptical/core/material/Conrady>
+#include <goptical/core/material/Sellmeier>
+#include <goptical/core/material/Abbe>
 
-#include <Goptical/Sys/System>
-#include <Goptical/Sys/OpticalSurface>
-#include <Goptical/Sys/SourcePoint>
-#include <Goptical/Sys/Image>
+#include <goptical/core/sys/System>
+#include <goptical/core/sys/Lens>
+#include <goptical/core/sys/OpticalSurface>
+#include <goptical/core/sys/SourcePoint>
+#include <goptical/core/sys/Image>
 
-#include <Goptical/Curve/Sphere>
-#include <Goptical/Shape/Disk>
+#include <goptical/core/curve/Sphere>
+#include <goptical/core/shape/Disk>
 
-#include <Goptical/Trace/Tracer>
-#include <Goptical/Trace/Result>
-#include <Goptical/Trace/Distribution>
-#include <Goptical/Trace/Sequence>
-#include <Goptical/Trace/Params>
+#include <goptical/core/trace/Tracer>
+#include <goptical/core/trace/Result>
+#include <goptical/core/trace/Distribution>
+#include <goptical/core/trace/Sequence>
+#include <goptical/core/trace/Params>
 
-#include <Goptical/Light/SpectralLine>
+#include <goptical/core/light/SpectralLine>
 
-#include <Goptical/Analysis/RayFan>
-#include <Goptical/Data/Plot>
+#include <goptical/core/analysis/RayFan>
+#include <goptical/core/data/Plot>
 
-#include <Goptical/Io/Rgb>
-#include <Goptical/Io/RendererSvg>
+#include <goptical/core/io/Rgb>
+#include <goptical/core/io/RendererSvg>
 
-using namespace Goptical;
+#include <goptical/core/ref>
+
+using namespace goptical;
 
 
 int main(void)
 {
-	Sys::System sys
+	sys::system mysys;
 
-	// code from examples/tessar_lens/tessar.cc:70
-	Sys::Lens     lens(Math::Vector3(0, 0, 0));
+	sys::Lens     mylens(math::Vector3(0, 0, 0));
 
 	//       roc,            ap.radius, thickness,
 	//
-	lens.add_surface(1/0.031186861,  14.934638, 4.627804137,
-        	ref<Material::Conrady>::create(1.7, 0.0, 0.0));
+	mylens.add_surface(1/0.031186861,  14.934638, 4.627804137,
+        	ref<material::Conrady>::create(1.7, 0.0, 0.0));
 	//
-	lens.add_surface(0,              14.934638, 5.417429465);
+	mylens.add_surface(0,              14.934638, 5.417429465);
 	//
-	lens.add_surface(1/-0.014065441, 12.766446, 3.728230979,
-        	ref<Material::Conrady>::create(1.7, 0.0, 0.0));
+	mylens.add_surface(1/-0.014065441, 12.766446, 3.728230979,
+        	ref<material::Conrady>::create(1.7, 0.0, 0.0));
 	//
-	lens.add_surface(1/0.034678487,  11.918098, 4.417903733);
+	mylens.add_surface(1/0.034678487,  11.918098, 4.417903733);
 	//
-	lens.add_stop   (                12.066273, 2.288913925);
-	lens.add_surface(0,              12.372318, 1.499288597,
-        	ref<Material::Conrady>::create(1.7, 0.0, 0.0));
+	mylens.add_stop   (                12.066273, 2.288913925);
+	mylens.add_surface(0,              12.372318, 1.499288597,
+        	ref<material::Conrady>::create(1.7, 0.0, 0.0));
 	//
-	lens.add_surface(1/0.035104369,  14.642815, 7.996205852,
-        	ref<Material::AbbeVd>::create(1.623770, 56.8998));
+	mylens.add_surface(1/0.035104369,  14.642815, 7.996205852,
+        	ref<material::AbbeVd>::create(1.623770, 56.8998));
 	//
-	lens.add_surface(1/-0.021187519, 14.642815, 85.243965130);
+	mylens.add_surface(1/-0.021187519, 14.642815, 85.243965130);
 	//
-	sys.add(lens);
+	mysys.add(mylens);
 
 }
