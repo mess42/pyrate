3089f1aa8706f01579d8e5998893e3c40b2ae193
joha2 <joha2@gmx.net>
2019-03-06 00:51:33 +0100

    demos: goptical system like benchmark

--- demos/demo_optimize_goptical.cc
+++ demos/demo_optimize_goptical.cc
@@ -40,29 +40,56 @@ int main(void)
 {
 	sys::system mysys;
 
-	sys::Lens     mylens(math::Vector3(0, 0, 0));
+	sys::SourcePoint source(sys::SourceAtInfinity, math::Vector3(0., 0., 1.));
+
+	sys::Lens     mylens(math::Vector3(0, 0, 2.0));
 
 	//       roc,            ap.radius, thickness,
 	//
-	mylens.add_surface(1/0.031186861,  14.934638, 4.627804137,
+	mylens.add_surface(-5.922,  2.0, 3.0,
         	ref<material::Conrady>::create(1.7, 0.0, 0.0));
 	//
-	mylens.add_surface(0,              14.934638, 5.417429465);
+	mylens.add_surface(-3.160,  2.0, 5.0, material::none);
 	//
-	mylens.add_surface(1/-0.014065441, 12.766446, 3.728230979,
+	mylens.add_surface(15.884, 2.0, 3.0,
         	ref<material::Conrady>::create(1.7, 0.0, 0.0));
 	//
-	mylens.add_surface(1/0.034678487,  11.918098, 4.417903733);
+	mylens.add_surface(-12.756,  2.0, 3.0, material::none);
 	//
-	mylens.add_stop   (                12.066273, 2.288913925);
-	mylens.add_surface(0,              12.372318, 1.499288597,
-        	ref<material::Conrady>::create(1.7, 0.0, 0.0));
+	mylens.add_stop   (                1.0, 2.0);
+	mylens.add_surface(3.125,              2.0, 3.0,
+        	ref<material::Conrady>::create(1.5, 0.0, 0.0));
 	//
-	mylens.add_surface(1/0.035104369,  14.642815, 7.996205852,
-        	ref<material::AbbeVd>::create(1.623770, 56.8998));
+	mylens.add_surface(1.479,  2.0, 19.0, material::none);
 	//
-	mylens.add_surface(1/-0.021187519, 14.642815, 85.243965130);
+	mylens.add_surface(0.0, 10.0, 0.0, material::none);
 	//
 	mysys.add(mylens);
+
+	trace::Sequence seq(mysys);
+	mysys.get_tracer_params().set_sequential_mode(seq);
+
+
+	io::RendererSvg renderer("layout.svg", 1024, 100);
+
+	// draw 2d system layout
+	mysys.draw_2d_fit(renderer);
+	mysys.draw_2d(renderer);
+
+	std::cout << mysys;
+	std::cout << seq;
+
+	// trace and draw rays from source
+
+	std::cout << "tracer init" << std::endl;
+	trace::tracer tracer(mysys);
+	std::cout << "1" << std::endl;
+
+	tracer.get_params().set_default_distribution(
+		trace::Distribution(trace::MeridionalDist, 1.0));
+	tracer.get_trace_result().set_generated_save_state(source);
+	tracer.trace();
+	tracer.get_trace_result().draw_2d(renderer);
+
 
 }
