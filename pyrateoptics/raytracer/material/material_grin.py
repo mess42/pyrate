97cc9bfcc87f5315a816293e840121cdfdb1220b
joha2 <joha2@gmx.net>
2020-03-08 11:26:00 +0100

    pylint3: raytracer/material/material_grin

--- pyrateoptics/raytracer/material/material_grin.py
+++ pyrateoptics/raytracer/material/material_grin.py
@@ -36,11 +36,14 @@ from .material_isotropic import IsotropicMaterial
 class IsotropicGrinMaterial(IsotropicMaterial):
 
     @classmethod
-    def p(cls, lc, fun, dfdx, dfdy, dfdz, parameterlist=[], name="", comment=""):
+    def p(cls, lc, fun, dfdx, dfdy, dfdz,
+          parameterlist=None, name="", comment=""):
+        if parameterlist is None:
+            parameterlist = []
         params = {}
         for (name, value) in parameterlist:
-            params[name] = FloatOptimizableVariable(FixedState(value),
-                                                         name=name)
+            params[name] = FloatOptimizableVariable(
+                FixedState(value), name=name)
         return cls({"comment": comment},
                    {"lc": lc, "nfunc": fun,  # FunctionObject
                     "dndx": dfdx, "dndy": dfdy, "dndz": dfdz,  # FunctionObject
@@ -49,7 +52,7 @@ class IsotropicGrinMaterial(IsotropicMaterial):
                     "ds": 0.1,  # Annotations
                     "energyviolation": 1e-3,  # Annotations
                     "params": params
-                    }, name=name)
+                   }, name=name)
 
     def getEpsilonTensor(self, x, wave=standard_wavelength):
         (num_dims, num_pts) = np.shape(x)
@@ -69,15 +72,20 @@ class IsotropicGrinMaterial(IsotropicMaterial):
     def inBoundary(self, x):
         return self.boundaryfunction(x)
 
-
     def symplecticintegrator(self, raybundle, nextSurface, tau):
 
         startpoint = self.lc.returnGlobalToLocalPoints(raybundle.x[-1])
-        startdirection = self.lc.returnGlobalToLocalDirections(raybundle.returnKtoD()[-1])
+        startdirection = self.lc.returnGlobalToLocalDirections(
+            raybundle.returnKtoD()[-1])
 
-        clist = [1.0/(2.0*(2.0 - 2.0**(1./3.))),(1.0-2.0**(1./3.))/(2.0*(2.0 - 2.0**(1./3.))),(1.0-2.0**(1./3.))/(2.0*(2.0 - 2.0**(1./3.))),1.0/(2.0*(2.0 - 2.0**(1./3.)))]
-        dlist = [1.0/(2.0 - 2.0**(1./3.)),(-2.0**(1./3.))/((2.0 - 2.0**(1./3.))),1.0/(2.0 - 2.0**(1./3.)),0.0]
-
+        clist = [1.0/(2.0*(2.0 - 2.0**(1./3.))),
+                 (1.0-2.0**(1./3.))/(2.0*(2.0 - 2.0**(1./3.))),
+                 (1.0-2.0**(1./3.))/(2.0*(2.0 - 2.0**(1./3.))),
+                 1.0/(2.0*(2.0 - 2.0**(1./3.)))]
+        dlist = [1.0/(2.0 - 2.0**(1./3.)),
+                 (-2.0**(1./3.))/((2.0 - 2.0**(1./3.))),
+                 1.0/(2.0 - 2.0**(1./3.)),
+                 0.0]
 
         optindstart = self.nfunc(startpoint, **self.params)
         positions = [1.*startpoint]
@@ -119,9 +127,6 @@ class IsotropicGrinMaterial(IsotropicMaterial):
             positions.append(newpos)
             velocities.append(newvel)
 
-
-
-
             # validity and finalization check
 
             totalenergy = np.sum(newvel**2) - np.sum(optin**2)
@@ -130,11 +135,16 @@ class IsotropicGrinMaterial(IsotropicMaterial):
             # and invalidate all rays
 
             if abs(totalenergy) > self.energyviolation:
-                self.warning('integration aborted due to energy violation: abs(' + str(totalenergy) + ') > ' + str(self.energyviolation))
+                self.warning('integration aborted due to energy violation: ' +
+                             'abs(' + str(totalenergy) + ') > ' +
+                             str(self.energyviolation))
                 self.warning('Please reduce integration step size.')
-                valid[:] = False # all rays with energy violation are not useful due to integration errors
+                valid[:] = False
+                # all rays with energy violation are not useful due to
+                # integration errors
 
-            self.debug("step(" + str(loopcount) + ") -> energy conservation violation: " + str(totalenergy))
+            self.debug("step(" + str(loopcount) + ") -> " +
+                       "energy conservation violation: " + str(totalenergy))
 
             xglobalnewpos = self.lc.returnLocalToGlobalPoints(newpos)
             xshape = nextSurface.shape.lc.returnGlobalToLocalPoints(xglobalnewpos)
@@ -148,12 +158,13 @@ class IsotropicGrinMaterial(IsotropicMaterial):
             final[True ^ valid] += True
             # all non valid rays are also final
 
-            updatedpos[:,True ^ final] = newpos[:,True ^ final]
-            updatedvel[:,True ^ final] = newvel[:,True ^ final]
+            updatedpos[:, True ^ final] = newpos[:, True ^ final]
+            updatedvel[:, True ^ final] = newvel[:, True ^ final]
 
-            k0 = 1. #2.*math.pi/raybundle.wave
+            k0 = 1.  # 2.*math.pi/raybundle.wave
             newk = k0*updatedvel/self.nfunc(updatedpos, **self.params)
-            Eapp = self.lc.returnLocalToGlobalDirections(self.calcEfield(newpos, None, newk, wave=raybundle.wave))
+            Eapp = self.lc.returnLocalToGlobalDirections(
+                self.calcEfield(newpos, None, newk, wave=raybundle.wave))
             kapp = self.lc.returnLocalToGlobalDirections(newk)
             xapp = self.lc.returnLocalToGlobalPoints(updatedpos)
 
@@ -161,69 +172,18 @@ class IsotropicGrinMaterial(IsotropicMaterial):
             momentatodraw.append(1.*updatedvel)
 
             raybundle.append(xapp, kapp, Eapp, valid)
-            # TODO: if pathlength of a certain ray is too long, mark as invalid and final
+            # TODO: if pathlength of a certain ray is too long,
+            # mark as invalid and final
 
             # for energy and phase space analysis
 
             energies.append(totalenergy)
 
-        return (positions, velocities, pointstodraw, momentatodraw, energies, valid)
-
+        return (positions, velocities, pointstodraw, momentatodraw,
+                energies, valid)
 
     def propagate(self, raybundle, nextSurface):
 
         self.symplecticintegrator(raybundle, nextSurface, self.ds)
 
         nextSurface.intersect(raybundle)
-
-
-#    def propagate(self, actualSurface, nextSurface, raybundle):
-#        startq = raybundle.o # linewise x, y, z values
-#        startp = raybundle.k
-
-#        (self.finalq, self.finalp, self.pointstodraw, self.momentatodraw, en, ph4d, validindices) = \
-#            self.symplecticintegrator(startq,
-#                                      startp,
-#                                      self.ds,
-#                                      0.0,
-#                                      actualSurface.getThickness(),
-#                                      actualSurface,
-#                                      nextSurface)
-
-        #for ind, pts in enumerate(self.pointstodraw):
-        #    FreeCAD.Console.PrintMessage("prop: " + str(ind) + ": " + str(pts) + "\n")
-
-        # TODO: z-values are not starting at zero and are therefore not relative to last surface
-        # TODO: starting in intersection points works but not hitting the final surface
-
-
-        #intersection = np.zeros_like(startq) # intersection are intersection points of nextsurface
-        #validindices = np.ones(np.shape(startq)[1], dtype=bool) # validindices are indices of surviving rays at nextsurface
-
-        #t = 1 # t is arc length of last ray
-        #normal = np.zeros_like(startq) # normal is array of normal vectors at nextsurface
-        #FreeCAD.Console.PrintMessage("ENERGY: "+str(en)+"\n finalq: "+str(self.finalq)+"\n finalp: "+str(self.finalp)+"\n")
-
-        # integrate from startq and startp to finalq and finalp and add in every step a raybundle to the final array
-        # if a ray hits the aperture boundary in x,y mark it as invalid
-        # define end loop conditions:
-        # - maximal arc length,
-        # - all valid rays hit nextSurface,
-        # - maximal loops (and all rays not at final surface are marked invalid)
-
-        # original start point self.finalq[-1], self.finalp[-1]
-
-#       intersection, t, normal, validindicesrefract = \
-#            nextSurface.shape.intersect(RayBundle(self.pointstodraw[-1], self.momentatodraw[-1], raybundle.rayID, raybundle.wave))
-#
-#        validindices *= validindicesrefract
-
-#        return intersection, t, normal, validindices
-        # intersection, t, normal, validindices, propraybundles
-        # TODO: Raybundles have to strong dependencies from surfaces. For every surface there is exactly one raybundle.
-        # This is not correct for grin media anymore, since a grin medium contains a collection of ray bundles. For every
-        # integration step one raybundle, but the standard raybundles are only defined with respect to their
-        # corresponding surface.
-
-
-
